ec269cd2d4b7e73cef7efff9e9913f71ba0d0a77
Kingsley Maina <mainakingsley7@gmail.com>
2023-11-27 08:46:24 +0300

    Done

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,16 @@
 # your code goes here!
+def countdown(x):
+    while x >= 1 :
+        print(f"{x} SECOND(S)!")
+        x -= 1
+    else :
+        print("HAPPY NEW YEAR!")
+countdown(10)
+
+def countdown_with_sleep(k):
+     while k >= 1 :
+        print(f"{k} SECOND(S)!")
+        k -= 1
+     else :
+        print("HAPPY NEW YEAR!")
+countdown_with_sleep(10)
